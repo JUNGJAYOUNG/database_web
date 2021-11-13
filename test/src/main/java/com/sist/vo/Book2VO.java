4e82cd935da80cf6b9b17882d7ad5bf832f5080d
JUNGJAYOUNG <jayoung1222@naver.com>
2021-11-13 23:02:47 +0900

    database_web start

---
+++ test/src/main/java/com/sist/vo/Book2VO.java
@@ -0,0 +1,24 @@
+package com.sist.vo;
+
+public class Book2VO {
+	private String ename;
+
+	public Book2VO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Book2VO(String ename) {
+		super();
+		this.ename = ename;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+}
